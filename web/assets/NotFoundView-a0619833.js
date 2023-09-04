e4313df63de4ff6febf7b9140e9a331d52733983
Richard Guerre <richardmguerre@gmail.com>
2023-09-04 11:15:46 +0800

    New build

--- web/assets/NotFoundView-a0619833.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-c866acf4.js";const e=()=>o("div",{children:o("h1",{children:"404"})});export{e as default};
---
+++ web/assets/RoutineSettingsView-a3d6c0a0.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-7daf0cae.js";const i=()=>t("div",{children:"Routine settings"});export{i as default};
