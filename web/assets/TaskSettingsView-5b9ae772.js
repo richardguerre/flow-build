05462eb769e829843d7adab4b9bbc98c71ac8d49
Richard Guerre <richardmguerre@gmail.com>
2023-10-03 14:36:02 +0800

    New build

--- web/assets/TaskSettingsView-5b9ae772.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-96445d93.js";const e=()=>s("div",{children:"Task settings"});export{e as default};
---
+++ web/assets/TaskSettingsView-678c2727.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-8bc38870.js";const e=()=>s("div",{children:"Task settings"});export{e as default};
