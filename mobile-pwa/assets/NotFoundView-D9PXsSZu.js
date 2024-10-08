e2e8d78b86162eb16601b67a2ae04efa6da19115
Richard Guerre <richardmguerre@gmail.com>
2024-10-08 19:46:40 +0200

    New build

---
+++ mobile-pwa/assets/NotFoundView-D9PXsSZu.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Dr1GzDe7.js";const t=()=>o.jsx("div",{children:o.jsx("h1",{children:"404"})});export{t as default};
--- mobile-pwa/assets/TestView-CjUd-OfU.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CbPDoP3U.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"TestView"})});export{t as default};
